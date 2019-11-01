64dad8c0df1dff96ad036cfe23688ba1175cb8a6
crew <jinhwan0515@gamil.com>
2019-11-01 20:21:13 +0900

    Initial commit

---
+++ src/main/java/xyz/just/board/JustBoardApplication.java
@@ -0,0 +1,13 @@
+package xyz.just.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustBoardApplication.class, args);
+    }
+
+}
